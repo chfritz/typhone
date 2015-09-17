286e6a0f0d5882723162f4d81c03c73d188efd67
Christian Fritz <christian.fritz@gmail.com>
2015-09-16 21:36:25 -0700

    Created a logo and added basic mobile-config.js file.
    - gimp file for logo included for editing later

---
+++ mobile-config.js
@@ -0,0 +1,14 @@
+App.info({
+  name: 'TyPhone',
+  description: 'An app that allows you to use your computer to control your phone (clipboard, sms, maps, browser, dial phone numbers, etc.).',
+  version: '0.0.1'
+});
+
+App.icons({
+    'android_ldpi': 'public/imgs/logo.png',
+    'android_mdpi': 'public/imgs/logo.png',
+    'android_hdpi': 'public/imgs/logo.png',
+    'android_xhdpi': 'public/imgs/logo.png',
+    'iphone': 'public/imgs/logo.png',
+    'ipad': 'public/imgs/logo.png'
+});
