71c14256ba72bb64c9f22356ec5cb33f2bea79bc
Christian Fritz <christian.fritz@gmail.com>
2015-10-03 21:46:44 -0700

    Updated script.

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ meteor build $DIR --server=http://typhone.xyz
 # keytool -genkey -alias typhone -keyalg RSA -keysize 2048 -validity 10000
 cd $DIR/android/
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 release-unsigned.apk typhone
+mv production.apk production.apk.bak
 ~/.meteor/android_bundle/android-sdk/build-tools/20.0.0/zipalign 4 release-unsigned.apk production.apk
 
 echo "$DIR/android/production.apk is your apk, now go to https://play.google.com/apps/publish"
