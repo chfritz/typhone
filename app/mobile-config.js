11327b2310312c48eb98ce4e9882f4cfb831d15c
Christian Fritz <christian.fritz@gmail.com>
2015-11-02 20:04:52 -0800

    New logo to pass App Store rules.

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -1,15 +1,15 @@
 App.info({
   name: 'TyPhone',
   description: 'Better connect your phone and your computer (transfer files wirelessly, set clipboard, send sms, start maps, open URLs, dial phone numbers, etc.).',
-  version: '0.0.5'
+  version: '0.0.6'
 });
 
 App.icons({
-    'android_ldpi': 'public/imgs/logo_hires_trans.png',
-    'android_mdpi': 'public/imgs/logo_hires_trans.png',
-    'android_hdpi': 'public/imgs/logo_hires_trans.png',
-    'android_xhdpi': 'public/imgs/logo_hires_trans.png',
+    'android_ldpi': 'public/imgs/logo_hires_trans_computer.png',
+    'android_mdpi': 'public/imgs/logo_hires_trans_computer.png',
+    'android_hdpi': 'public/imgs/logo_hires_trans_computer.png',
+    'android_xhdpi': 'public/imgs/logo_hires_trans_computer.png',
     'iphone': 'public/imgs/logo_hires_trans_76x76.png',
     'iphone_2x': 'public/imgs/logo_hires_trans_120x120.png',
-    'ipad': 'public/imgs/logo_hires_trans.png'
+    'ipad': 'public/imgs/logo_hires_trans_computer.png'
 });
